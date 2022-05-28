51f2b95e3c56c061c1259c47448a8136118840a2
git-antonyuk <as.antonuk@gmail.com>
2022-05-28 16:04:51 +0200

    init

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div>
+      {{ count }}
+    </div>
+    <button @click="increment">Increment</button>
+  </div>
+</template>
+
+<script setup>
+const count = ref(0);
+const increment = () => count.value++;
+</script>
